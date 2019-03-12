e2f4d74f64a258489cec4dad1d58f5530aa06eb9
Bartosz Meglicki <bmegli@op.pl>
2019-03-12 23:08:01 +0100

    rewrite validation condition that worked for funny reasons
    There was an error in parenthesis placement in one of validation conditions.
    By coincidence the code was equivalent but only due to fact that the compared
    constant was 1 and it was tested for equality with complex boolean condition.
    
    - also remove excess whitespaces
    - fix some formatting

--- vmu931.c
+++ vmu931.c
@@ -1,11 +1,11 @@
 /*
  * vmu931 library header
  *
- * Copyright 2018 (C) Bartosz Meglicki <meglickib@gmail.com>
+ * Copyright 2018-2019 (C) Bartosz Meglicki <meglickib@gmail.com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  */
 
@@ -34,17 +34,17 @@ enum {VMU_READ_TIMEOUT_MS=50,
       VMU_SELFTEST_TIMEOUT_MS=2000, //typically around 1,5 seconds and garbage for less than 100 ms
       VMU_STATUS_TIMEOUT_MS=500, //typically 40 ms, but it happends that status fails for some time after sending commands
 	  VMU_POST_COMMAND_DELAY_MS=200, //after commands device sometimes doesn't respond for a while
-	  VMU_COMMAND_INTERBYTE_DELAY_MS=1 //variense vmu_utils.h mentions a bug, wait at least 1 ms between command chars 
+	  VMU_COMMAND_INTERBYTE_DELAY_MS=1 //variense vmu_utils.h mentions a bug, wait at least 1 ms between command chars
 	  };
 
 /* NON TUNABLE CONSTANTS */
 
 // start end delimeters
 enum {VMU_START_OF_MESSAGE_STRING=0x02, VMU_END_OF_MESSAGE_STRING=0x03,
-	  VMU_START_OF_MESSAGE_DATA=0x01, VMU_END_OF_MESSAGE_DATA=0x04}; 
-	  
-// payload sizes	  
-enum {VMU_TXYZ_MSG_SIZE=20, VMU_TWXYZ_MSG_SIZE=24, VMU_HEAD_MSG_SIZE=12, VMU_STATUS_MSG_SIZE=11, VMU_COMMAND_SIZE=4}; 
+	  VMU_START_OF_MESSAGE_DATA=0x01, VMU_END_OF_MESSAGE_DATA=0x04};
+
+// payload sizes
+enum {VMU_TXYZ_MSG_SIZE=20, VMU_TWXYZ_MSG_SIZE=24, VMU_HEAD_MSG_SIZE=12, VMU_STATUS_MSG_SIZE=11, VMU_COMMAND_SIZE=4};
 enum {VMU_NON_PAYLOAD_SIZE=4};
 
 // offsets
@@ -146,7 +146,7 @@ struct vmu *vmu_init(const char *tty)
 
 	v->buffer_bytes=0;
 	v->data_pending=0;
-	
+
 	if ( (v->fd=open(tty, O_RDWR)) ==-1 )
 	{
 		free(v);
@@ -158,10 +158,10 @@ struct vmu *vmu_init(const char *tty)
 		close(v->fd);
 		free(v);
 		return NULL;
-	}	
-	
+	}
+
 	v->actual_termios.c_iflag=v->actual_termios.c_oflag=v->actual_termios.c_lflag=0;
-	v->actual_termios.c_cflag=CS8|CREAD|CLOCAL; //8 bit characters	
+	v->actual_termios.c_cflag=CS8|CREAD|CLOCAL; //8 bit characters
 	v->actual_termios.c_cc[VMIN]=1;
 	v->actual_termios.c_cc[VTIME]=0;
 
@@ -172,23 +172,23 @@ struct vmu *vmu_init(const char *tty)
 		free(v);
 		return NULL;
 	}
-	
+
 	if(tcflush(v->fd, TCIOFLUSH) < 0)
 	{
 		close(v->fd);
 		free(v);
 		return NULL;
 	}
-	
+
 	// from man (TO DO)
 	// Note that tcsetattr() returns success if any of the  requested  changes
-    // could  be  successfully  carried  out.  Therefore, when making multiple
-    // changes it may be necessary to follow this call with a further call  to
-    // tcgetattr() to check that all changes have been performed successfully.
+	// could  be  successfully  carried  out.  Therefore, when making multiple
+	// changes it may be necessary to follow this call with a further call  to
+	// tcgetattr() to check that all changes have been performed successfully.
 	//
-    // this is still edge case to consider, some settings may have not been made
+	// this is still edge case to consider, some settings may have not been made
 	// solution tcgetattr and check settings we made for equality
-	
+
 	return v;
 }
 
@@ -210,7 +210,7 @@ int vmu_close(struct vmu *v)
 
 	if(error)
 		return VMU_ERROR;
-	
+
 	return VMU_OK;
 }
 
@@ -223,10 +223,10 @@ int vmu_accel(struct vmu *v, struct vmu_txyz *data, int size)
 	struct vmu_data vdata = {0};
 	vdata.accel=data;
 	vdata.size.accel=size;
-	
+
 	if( (ret=vmu_read_all(v, &vdata)) == VMU_ERROR )
 		return VMU_ERROR;
-	
+
 	if(ret == VMU_DATA_PENDING)
 		return size+1;
 
@@ -240,10 +240,10 @@ int vmu_gyro(struct vmu *v, struct vmu_txyz *data, int size)
 	struct vmu_data vdata = {0};
 	vdata.gyro=data;
 	vdata.size.gyro=size;
-	
+
 	if( (ret=vmu_read_all(v, &vdata)) == VMU_ERROR )
 		return VMU_ERROR;
-	
+
 	if(ret == VMU_DATA_PENDING)
 		return size+1;
 
@@ -257,10 +257,10 @@ int vmu_mag(struct vmu *v, struct vmu_txyz *data, int size)
 	struct vmu_data vdata = {0};
 	vdata.mag=data;
 	vdata.size.mag=size;
-	
+
 	if( (ret=vmu_read_all(v, &vdata)) == VMU_ERROR )
 		return VMU_ERROR;
-	
+
 	if(ret == VMU_DATA_PENDING)
 		return size+1;
 
@@ -274,10 +274,10 @@ int vmu_euler(struct vmu *v, struct vmu_txyz *data, int size)
 	struct vmu_data vdata = {0};
 	vdata.euler=data;
 	vdata.size.euler=size;
-	
+
 	if( (ret=vmu_read_all(v, &vdata)) == VMU_ERROR )
 		return VMU_ERROR;
-	
+
 	if(ret == VMU_DATA_PENDING)
 		return size+1;
 
@@ -291,10 +291,10 @@ int vmu_quat(struct vmu *v, struct vmu_twxyz *data, int size)
 	struct vmu_data vdata = {0};
 	vdata.quat=data;
 	vdata.size.quat=size;
-	
+
 	if( (ret=vmu_read_all(v, &vdata)) == VMU_ERROR )
 		return VMU_ERROR;
-	
+
 	if(ret == VMU_DATA_PENDING)
 		return size+1;
 
@@ -308,10 +308,10 @@ int vmu_head(struct vmu *v, struct vmu_tx *data, int size)
 	struct vmu_data vdata = {0};
 	vdata.head=data;
 	vdata.size.head=size;
-	
+
 	if( (ret=vmu_read_all(v, &vdata)) == VMU_ERROR )
 		return VMU_ERROR;
-	
+
 	if(ret == VMU_DATA_PENDING)
 		return size+1;
 
@@ -328,7 +328,7 @@ int vmu_read_all(struct vmu* v, struct vmu_data *data)
 		data->size=counters;
 		return VMU_ERROR;
 	}
-	
+
 	while( (valid=validate_message(v, offset)) != VMU_NEED_MORE_DATA  )
 	{
 		if(valid == VMU_INVALID_MESSAGE)
@@ -338,16 +338,16 @@ int vmu_read_all(struct vmu* v, struct vmu_data *data)
 		}
 		//otherwise VMU_VALID_MESSAGE
 		if( (msg_process_status=process_message(v->buffer+offset, data, &counters)) == VMU_NO_SPACE_IN_USER_ARRAY)
-			break; 
+			break;
 		//otherwise VMU_MESSAGE_PROCESSED
 		offset+=v->buffer[offset+VMU_MESSAGE_SIZE_OFFSET];
 	}
-	
+
 	memmove(v->buffer, v->buffer+offset, v->buffer_bytes-offset);
 	v->buffer_bytes -= offset;
-	
-	data->size = counters; 
-		
+
+	data->size = counters;
+
 	if(msg_process_status == VMU_NO_SPACE_IN_USER_ARRAY)
 	{
 		v->data_pending=1;
@@ -363,34 +363,34 @@ int vmu_read_all(struct vmu* v, struct vmu_data *data)
 // returns VMU_INVALID_MESSAGE or VMU_NEED_MORE_DATA or VMU_VALID_MESSAGE
 static int validate_message(struct vmu *v, int from)
 {
-	int pending_bytes=v->buffer_bytes-from;
+	unsigned int pending_bytes=v->buffer_bytes-from;
 	uint8_t msg_start, msg_size=UINT8_MAX, msg_end, msg_type;
-	
+
 	if(pending_bytes == 0)
 		return VMU_NEED_MORE_DATA;
-	
+
 	if(pending_bytes >= 1)
 	{
 		msg_start=v->buffer[from];
 		if(!is_valid_message_start(msg_start))
-			return VMU_INVALID_MESSAGE; 
-	}	
-	
+			return VMU_INVALID_MESSAGE;
+	}
+
 	if(pending_bytes >= 3)
 	{
 		msg_size=v->buffer[from+1];
 		msg_type=v->buffer[from+2];
-		
+
 		if(!is_valid_length_for_message_type(msg_start, msg_type, msg_size))
 			return VMU_INVALID_MESSAGE;
 	}
-		
+
 	if(pending_bytes >= msg_size ) //start, length, type/reserved, end so we have end of message
 	{
 		msg_end=v->buffer[from + msg_size -1];
-		
+
 		if(!is_valid_message_start_end(msg_start, msg_end))
-			return VMU_INVALID_MESSAGE; 
+			return VMU_INVALID_MESSAGE;
 
 		// if we got that far:
 		// - message has correct start
@@ -399,8 +399,8 @@ static int validate_message(struct vmu *v, int from)
 		// we conclude that it is a valid message
 		return VMU_VAlID_MESSAGE;
 	}
-	
-	return VMU_NEED_MORE_DATA;	
+
+	return VMU_NEED_MORE_DATA;
 }
 
 static int is_valid_message_start(uint8_t c)
@@ -416,11 +416,11 @@ static int is_valid_message_start_end(uint8_t msg_start, uint8_t msg_end)
 
 static int is_valid_length_for_message_type(uint8_t msg_start,uint8_t msg_type, uint8_t msg_length)
 {
-	return msg_start == (VMU_START_OF_MESSAGE_DATA &&
+	return (msg_start == VMU_START_OF_MESSAGE_DATA &&
 	( ( (msg_type == 'a' || msg_type=='g' || msg_type=='c' || msg_type=='e') && msg_length==VMU_TXYZ_MSG_SIZE ) ||
 		(msg_type == 'q' && msg_length==VMU_TWXYZ_MSG_SIZE) || (msg_type=='h' && msg_length==VMU_HEAD_MSG_SIZE) ||
 		(msg_type == 's' && msg_length==VMU_STATUS_MSG_SIZE)) )
-		|| 
+		||
 	    (msg_start == VMU_START_OF_MESSAGE_STRING && msg_length > 0);
 }
 
@@ -431,22 +431,22 @@ static int process_message(uint8_t *msg, struct vmu_data *data, struct vmu_size 
 {
 	const uint8_t msg_start=msg[VMU_START_OF_MESSAGE_OFFSET];
 	const uint8_t msg_type=msg[VMU_MESSAGE_TYPE_OFFSET];
-		
+
 	if(msg_start == VMU_START_OF_MESSAGE_STRING)
 	{
 		if(data->size.text == 0) //ignore the message of not collected type
-			return VMU_MESSAGE_PROCESSED; 
+			return VMU_MESSAGE_PROCESSED;
 
 		if(counters->text >= data->size.text)
 			return VMU_NO_SPACE_IN_USER_ARRAY;
-		
+
 		//otherwise we have place for the message
-		decode_message_string(msg, data->text + counters->text); 
+		decode_message_string(msg, data->text + counters->text);
 		++counters->text;
 
 		return VMU_MESSAGE_PROCESSED;
 	}
-	
+
 	//otherwise msg_start == VMU_START_OF_MESSAGE_DATA
 	switch(msg_type)
 	{
@@ -455,9 +455,9 @@ static int process_message(uint8_t *msg, struct vmu_data *data, struct vmu_size 
 				return VMU_MESSAGE_PROCESSED;
 			if(counters->accel >= data->size.accel)
 				return VMU_NO_SPACE_IN_USER_ARRAY;
-			
+
 			decode_message_txyz(msg, data->accel + counters->accel);
-			
+
 			++counters->accel;
 			break;
 		case 'g':
@@ -465,9 +465,9 @@ static int process_message(uint8_t *msg, struct vmu_data *data, struct vmu_size 
 				return VMU_MESSAGE_PROCESSED;
 			if(counters->gyro >= data->size.gyro)
 				return VMU_NO_SPACE_IN_USER_ARRAY;
-			
+
 			decode_message_txyz(msg, data->gyro + counters->gyro);
-			
+
 			++counters->gyro;
 			break;
 		case 'c':
@@ -475,9 +475,9 @@ static int process_message(uint8_t *msg, struct vmu_data *data, struct vmu_size 
 				return VMU_MESSAGE_PROCESSED;
 			if(counters->mag >= data->size.mag)
 				return VMU_NO_SPACE_IN_USER_ARRAY;
-			
+
 			decode_message_txyz(msg, data->mag + counters->mag);
-			
+
 			++counters->mag;
 			break;
 		case 'e':
@@ -485,9 +485,9 @@ static int process_message(uint8_t *msg, struct vmu_data *data, struct vmu_size 
 				return VMU_MESSAGE_PROCESSED;
 			if(counters->euler >= data->size.euler)
 				return VMU_NO_SPACE_IN_USER_ARRAY;
-			
+
 			decode_message_txyz(msg, data->euler + counters->euler);
-			
+
 			++counters->euler;
 			break;
 		case 'q':
@@ -495,9 +495,9 @@ static int process_message(uint8_t *msg, struct vmu_data *data, struct vmu_size 
 				return VMU_MESSAGE_PROCESSED;
 			if(counters->quat >= data->size.quat)
 				return VMU_NO_SPACE_IN_USER_ARRAY;
-			
+
 			decode_message_twxyz(msg, data->quat + counters->quat);
-			
+
 			++counters->quat;
 			break;
 		case 'h':
@@ -505,9 +505,9 @@ static int process_message(uint8_t *msg, struct vmu_data *data, struct vmu_size 
 				return VMU_MESSAGE_PROCESSED;
 			if(counters->head >= data->size.head)
 				return VMU_NO_SPACE_IN_USER_ARRAY;
-			
+
 			decode_message_tx(msg, data->head + counters->head);
-			
+
 			++counters->head;
 			break;
 		case 's':
@@ -523,9 +523,9 @@ static int process_message(uint8_t *msg, struct vmu_data *data, struct vmu_size 
 		default:
 			;//fprintf(stderr, "unsupported message type: %c\n", msg_type);
 	}
-	
+
 	//if we got that far the message is of unsupported type
-	return VMU_MESSAGE_PROCESSED; 
+	return VMU_MESSAGE_PROCESSED;
 }
 
 /* Message level decoding */
@@ -566,7 +566,7 @@ static void decode_message_status(uint8_t *msg, struct vmu_status *data)
 	data->sensors=payload[0];
 	data->resolution=payload[1];
 	data->low_rate=payload[2];
-	data->stream=decode_uint32(payload+3);	
+	data->stream=decode_uint32(payload+3);
 }
 
 /* Data type level decoding */
@@ -584,7 +584,7 @@ static float decode_float(uint8_t *encoded)
 	memcpy(&temp, encoded, sizeof(temp));
 	temp=be32toh(temp);
 	memcpy(&tempf, &temp, sizeof(temp));
-	return tempf;	
+	return tempf;
 }
 
 /* Stream settings functions */
@@ -600,16 +600,16 @@ int vmu_stream(struct vmu *v, uint32_t stream)
 	}
 	if(stream == status.stream)
 		return VMU_OK; //nothing to do
-	
+
 	//toggle streaming to match the requested state
 	if( (stream & VMU_STREAM_ACCEL) != (status.stream & VMU_STREAM_ACCEL) )
 		if(send(v, "vara") == VMU_ERROR)
 			 return VMU_ERROR;
-			 
+
 	if( (stream & VMU_STREAM_GYRO) != (status.stream & VMU_STREAM_GYRO) )
 		if(send(v, "varg") == VMU_ERROR)
 			 return VMU_ERROR;
-			 
+
 	if( (stream & VMU_STREAM_MAG) != (status.stream & VMU_STREAM_MAG) )
 		if(send(v, "varc") == VMU_ERROR)
 			 return VMU_ERROR;
@@ -625,9 +625,9 @@ int vmu_stream(struct vmu *v, uint32_t stream)
 	if( (stream & VMU_STREAM_HEAD) != (status.stream & VMU_STREAM_HEAD) )
 		if(send(v, "varh") == VMU_ERROR)
 			 return VMU_ERROR;
-			
-	sleep_ms(VMU_POST_COMMAND_DELAY_MS);		
-	
+
+	sleep_ms(VMU_POST_COMMAND_DELAY_MS);
+
 	if(vmu_status(v, &status) == VMU_ERROR)
 	{
 		//perror("status failed\n");
@@ -636,35 +636,35 @@ int vmu_stream(struct vmu *v, uint32_t stream)
 
 	if(stream == status.stream)
 		return VMU_OK;
-	
+
 	//printf("stream failed\n");
-	return VMU_ERROR;		
+	return VMU_ERROR;
 }
 
 int vmu_resolution(struct vmu *v, uint32_t resolution)
 {
 	struct vmu_status status;
-	
+
 	if(validate_resolution(resolution) == VMU_ERROR)
-	{	
+	{
 		errno = EINVAL;
 		return VMU_ERROR;
 	}
-	
+
 	if(vmu_status(v, &status) == VMU_ERROR)
 		return VMU_ERROR; //errno with some IO error or EAGAIN on timeout
-	
+
 	if( resolution_matches(resolution, status.resolution) == VMU_OK)
 		return VMU_OK; //nothing to be done
-		
+
 	//streaming heading, euler or quaterion forces 2000 dps gyro and 2 g accelerometer, no changes allowed
 	if( (status.stream & VMU_STREAM_HEAD) || (status.stream & VMU_STREAM_EULER) || (status.stream & VMU_STREAM_QUAT))
-	{ 	
+	{
 		errno = ENOTSUP;
 		return VMU_ERROR;
 	}
 
-	//if we got that far we need to set requested resolution	
+	//if we got that far we need to set requested resolution
 	if( (resolution & VMU_RESOLUTION_GYRO_250DPS) && (send(v, "var0") == VMU_ERROR) )
 			return VMU_ERROR;
 	if( (resolution & VMU_RESOLUTION_GYRO_500DPS) && (send(v, "var1") == VMU_ERROR) )
@@ -686,7 +686,7 @@ int vmu_resolution(struct vmu *v, uint32_t resolution)
 	//finally confirm that resolution was set
 	if(vmu_status(v, &status) == VMU_ERROR)
 		return VMU_ERROR; //errno with some IO error or EAGAIN on timeout
-	
+
 	return resolution_matches(resolution, status.resolution);
 }
 
@@ -707,30 +707,30 @@ static int validate_resolution(uint32_t resolution)
 	((resolution & VMU_RESOLUTION_GYRO_500DPS) !=0) +
 	((resolution & VMU_RESOLUTION_GYRO_1000DPS) !=0) +
 	((resolution & VMU_RESOLUTION_GYRO_2000DPS) !=0);
-	
+
 	if(suma>1 || sumg>1 || (suma+sumg) == 0)
 		return VMU_ERROR;
-	
+
 	return VMU_OK;
 }
 
 //prerequisities:
-//- requested resolution validated (at least ACCEL or GYRO set, sane combination) 
+//- requested resolution validated (at least ACCEL or GYRO set, sane combination)
 //returns:
 // VMU_SUCCES if requested resolution matches current resolution
 // VMU_ERROR otherwise
 static int resolution_matches(uint32_t requested, uint32_t current)
 {
-	if(requested == current) 
+	if(requested == current)
 		return VMU_OK; //nothing to be done
-	
+
 	if( !(requested & VMU_RESOLUTION_ACCEL_MASK) )
 		if( (requested & VMU_RESOLUTION_GYRO_MASK) ==  (current & VMU_RESOLUTION_GYRO_MASK) )
 			return VMU_OK;
-		
+
 	if( !(requested & VMU_RESOLUTION_GYRO_MASK) )
 		if( (requested & VMU_RESOLUTION_ACCEL_MASK) ==  (current & VMU_RESOLUTION_ACCEL_MASK) )
-			return VMU_OK;			
+			return VMU_OK;
 
 	return VMU_ERROR;
 }
@@ -738,7 +738,7 @@ static int resolution_matches(uint32_t requested, uint32_t current)
 /* Test, calibration and device status */
 
 // blocks for 2 seconds
-// returns: 
+// returns:
 // - VMU_OK on success, you may query data->text for more information
 // - VMU_ERROR on IO error, calibration error or timeout, query data->text for the reason
 int vmu_selftest(struct vmu* v, struct vmu_text *data)
@@ -756,7 +756,7 @@ int vmu_selftest(struct vmu* v, struct vmu_text *data)
 }
 
 // blocks for 4 seconds
-// returns: 
+// returns:
 // - VMU_OK on success, you may query data->text for more information
 // - VMU_ERROR on IO error, calibration error or timeout, query data->text for the reason
 int vmu_calibrate(struct vmu *v, struct vmu_text *data)
@@ -766,7 +766,7 @@ int vmu_calibrate(struct vmu *v, struct vmu_text *data)
 
 	if(send(v, "varl") == VMU_ERROR)
 		return VMU_ERROR;
-	
+
 	// Typically vmu returns these sequences of messages:
 	// 1: Calibration started. | Calibration completed.
 	// 2: Calibration started. | Gyro failed. | Accel failed. | Calibration cannot be completed due to device failure.
@@ -777,22 +777,22 @@ static int wait_string(struct vmu *v, int total_timeout_ms, const char *awaited_
 {
 	const double TIMEOUT_S=total_timeout_ms/1000.0;
 	time_t start;
-	int bytes_copied=0, len, copy_max;	
+	int bytes_copied=0, len, copy_max;
 	struct vmu_data vmu_data = {0};
-	struct vmu_text vmu_text;	
+	struct vmu_text vmu_text;
 	int status=VMU_ERROR, awaited_string_length=strlen(awaited_string);
 
 	vmu_data.text=&vmu_text;
-	
+
 	if(data)
 		data->text[0]='\0';
-		
+
 	start=time(NULL);
 	//wait even if finished, VMU returns garbage for a while after calibration/test
 	while( difftime(time(NULL), start)<TIMEOUT_S )
 	{
 		vmu_data.size.text=1;
-		
+
 		if( vmu_read_all(v, &vmu_data) == VMU_ERROR && errno != EAGAIN )
 			return VMU_ERROR;
 
@@ -805,15 +805,15 @@ static int wait_string(struct vmu *v, int total_timeout_ms, const char *awaited_
 
 		if(data) //concatenate messages from vmu for user
 		{
-			len=strlen(vmu_data.text->text);	
-			copy_max = (bytes_copied+len < VMU_MAX_STRING_SIZE -1 ) ? len : VMU_MAX_STRING_SIZE - 1 - bytes_copied;  				
+			len=strlen(vmu_data.text->text);
+			copy_max = (bytes_copied+len < VMU_MAX_STRING_SIZE -1 ) ? len : VMU_MAX_STRING_SIZE - 1 - bytes_copied;
 			strncpy(data->text+bytes_copied, vmu_data.text->text, copy_max);
 			bytes_copied += copy_max;
 			data->text[bytes_copied]='\0';
 		}
 	}
-				
-	return status;	
+
+	return status;
 }
 
 int vmu_status(struct vmu *v, struct vmu_status *data)
@@ -822,16 +822,16 @@ int vmu_status(struct vmu *v, struct vmu_status *data)
 	time_t start;
 	struct vmu_data vmu_data = {0};
 	vmu_data.status=data;
-	
+
 	if(send(v, "vars") == VMU_ERROR)
 		return VMU_ERROR;
 
 	start=time(NULL);
-	//typically 40 ms	
+	//typically 40 ms
 	while( difftime(time(NULL), start)<TIMEOUT_S )
 	{
 		vmu_data.size.status=1;
-		
+
 		if( vmu_read_all(v, &vmu_data) == VMU_ERROR && errno != EAGAIN )
 			return VMU_ERROR;
 
@@ -857,8 +857,8 @@ static int recv(struct vmu *v, int timeout_ms)
 	FD_ZERO(&rfds);
 	FD_SET(v->fd, &rfds);
 
-	tv.tv_usec = VMU_READ_TIMEOUT_MS*1000; 	
-		
+	tv.tv_usec = VMU_READ_TIMEOUT_MS*1000;
+
 	if( (ret = select(v->fd+1, &rfds, NULL, NULL, &tv)) < 0 )
 		return VMU_ERROR;
 
@@ -867,7 +867,7 @@ static int recv(struct vmu *v, int timeout_ms)
 		errno = EAGAIN;
 		return VMU_ERROR;
 	}
-	
+
 	if( (ret = read(v->fd, v->buffer+v->buffer_bytes, VMU_BUFFER_SIZE-v->buffer_bytes )) < 0 )
 		return VMU_ERROR;
 	if( ret == 0 )
@@ -877,7 +877,7 @@ static int recv(struct vmu *v, int timeout_ms)
 	}
 
 	v->buffer_bytes += ret;
-	
+
 	return VMU_OK;
 }
 
@@ -885,11 +885,11 @@ static int recv(struct vmu *v, int timeout_ms)
 /* prerequisities:
  * - vmu931_init called successfully
  * - command of length VMU_COMMAND_SIZE (4)
- * 
+ *
  * returns:
  * - VMU_OK on success
  * - VMU_ERROR on error and errno is set
-*/  
+*/
 static int send(struct vmu *v, const char *command)
 {
 	int bytes_sent=0, status;
